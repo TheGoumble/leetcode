29b65c28f1ff7d5df0b49bfbdf2276746edfc1f8
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-23 13:06:16 -0400

    finished the way of doing twosums

--- twoSum.js
+++ twoSum.js
@@ -7,19 +7,29 @@
 @return {number[]}
 */
 
-var twoSum = function (nums, target) {
-  
-    // Loop throught the index
-  for (let i = 0; i < nums.length; i++) {
+// var twoSum = function (nums, target) {
+//   // Loop throught the index
 
-    //now check if the index can be sum with the other index with a loop 
-    for(let x = 0; x < nums.length - i; x++){
-        if(num[i]+num[x] === target){
-            let x = [i, x]
-            return x
-        }
-    }
-  }
-}
+//   for (let a = 0; a < nums.length; a++) {
+//     //now check if the index can be sum with the other index with a loop
 
+//     for (let b = 0; b < nums.length; b++) {
+//       //condition to check if the current numbers equal target 
 
+//       if (b != a && nums[a] + nums[b] === target ) {
+//         return [a, b]
+//        }
+//     }
+//   }
+// }
+// let number = [5, 4, 4, 3]
+// console.log(twoSum(number, 8))
+
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    
+};
