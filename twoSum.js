9ef7748ec8cef6d04e866f924feeee2b2e0a9957
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-25 12:57:52 -0400

    hash solution of the same problem

--- twoSum.js
+++ twoSum.js
@@ -14,7 +14,7 @@
 //     //now check if the index can be sum with the other index with a loop
 
 //     for (let b = 0; b < nums.length; b++) {
-//       //condition to check if the current numbers equal target 
+//       //condition to check if the current numbers equal target
 
 //       if (b != a && nums[a] + nums[b] === target ) {
 //         return [a, b]
@@ -22,14 +22,21 @@
 //     }
 //   }
 // }
-// let number = [5, 4, 4, 3]
-// console.log(twoSum(number, 8))
 
-/*
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
- var twoSum = function(nums, target) {
-    
-};
+let twoSum = (nums, target) => {
+  let storageHash = {}
+  let number = []
+  let i = 0
+  for (i in nums) {
+    let addend = target - nums[i]
+
+    if (addend in storageHash) {
+      number.push(nums.indexOf(addend), i)
+    }
+    storageHash[nums[i]] = i
+  }
+  return number
+}
+
+let number = [5, 4, 4, 3]
+console.log(twoSum([2, 5, 7, 11, 15], 9))
